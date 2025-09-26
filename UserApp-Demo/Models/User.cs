71d52970cad96c1cdae3c01cb982f0079980e321
Mr_Sekiro <abdullah811001@gmail.com>
2025-09-26 17:05:16 +0300

    Verfiy Email OTP

--- UserApp-Demo/Models/User.cs
+++ UserApp-Demo/Models/User.cs
@@ -5,5 +5,7 @@ namespace UserApp_Demo.Models
     public class User : IdentityUser
     {
         public string FullName { get; set; } = null!;
+        public string? EmailOtp { get; set; }
+        public DateTime? OtpExpiryTime { get; set; }
     }
 }
